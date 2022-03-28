7658f1d131a5ec689ba03786ce2d07f01660bd0b
Esther Marie <24815585+EstherMarie@users.noreply.github.com>
2022-03-27 21:46:06 -0300

    Solve exercise 03 - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (array) {
+	let reverseArray = [];
+	for (let i = array.length; i >= 0; i--) {
+		reverseArray.push(array[i]);
+	}
+	return reverseArray.join('');
 };
 
 // Do not edit below this line
